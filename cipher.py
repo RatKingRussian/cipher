075d2a7dcbc5f4cfd44fe18a5969feeae41c13ff
RatKingRussian <ratkingrussian@gmail.com>
2024-12-29 16:37:22 -0600

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,16 +1,27 @@
-text = 'Hello Zaira'
-shift = 3
-
-def caesar():
+def vigenere(message, key, direction):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
-    for char in text.lower():
+    for char in message.lower():
+    
+        # Append space to the message
         if char == ' ':
-            encrypted_text += char
-        else:
+            final_message += char
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + shift) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    print('plain text:', text)
-    print('encrypted text:', encrypted_text)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+encryption = vigenere(text, custom_key, 1)
+print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
+
